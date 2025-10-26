0c7ce00156612cd215399b07572966cd0070506c
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-10-26 21:34:04 +0200

    Enhance normalize function in preprocess.py with detailed docstring explaining Hebrew text normalization to Unicode NFD and character replacements for Geresh and Gershayim.

--- src/preprocess.py
+++ src/preprocess.py
@@ -6,6 +6,10 @@ See https://en.wikipedia.org/wiki/Unicode_and_HTML_for_the_Hebrew_alphabet#Compa
 import unicodedata
 
 def normalize(text: str) -> str:
+    """
+    Normalize Hebrew text to Unicode NFD form
+    Normalize Hebrew Geresh and Gershayim to apostrophe and double quote
+    """
     text = unicodedata.normalize('NFD', text)
     text = text.replace('\u05f3', "'") # Geresh -> apostrophe
     text = text.replace('\u05f4', '"') # Gershayim -> double quote
