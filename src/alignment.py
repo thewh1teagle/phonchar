dc24c617a6c75d41e18f48a7d6ffc51c40e4bcc6
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-10-26 22:01:13 +0200

    Refactor align_word function in alignment.py to return lists of word and IPA parts; update config.py with character mappings for Geresh and Gershayim, and enhance preprocess.py to remove diacritics during normalization.

--- src/alignment.py
+++ src/alignment.py
@@ -1,18 +1,27 @@
 """
 Align word and IPA to pairs of character and phoneme
 """
-from src.config import VOWELS, CONSONANTS, NONE, STRESS
+from src import config
 from src.preprocess import normalize
 
-def align_word(word: str, ipa: str) -> list[tuple[str, str]]:
+def align_word(word: str, ipa: str) -> list[list[str], list[str]]:
     word = normalize(word)
     
-    pairs = []
-    segmented_ipa = ''
+    word_parts = list(word)
+    ipa_parts = []
     
-    word_index = 0
-    ipa_index = 0
-    
+    index = 0
+    while True:
+        cur_char = word[index]
+        next_char = word[index + 1] if index + 1 < len(word) else None
+        cur_phoneme = ipa[index]
+        next_phoneme = ipa[index + 1] if index + 1 < len(ipa) else None
 
+        if next_char == config.GERESH or next_char == config.GERSHAYIM:
+            cur_phoneme = config.CHAR_TO_PHONEME[f"{cur_char}{next_char}"]
+            index += 2
+        else:
+            cur_phoneme = config.CHAR_TO_PHONEME[cur_char]
+            index += 1
 
-    return [word, segmented_ipa]
+    return [word_parts, ipa_parts]
