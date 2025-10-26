da2d63bb06505d788dc82b192440c0277930ce40
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-10-26 21:04:21 +0200

    Integrate text normalization in align_word function to preprocess input words before alignment.

--- src/alignment.py
+++ src/alignment.py
@@ -2,8 +2,11 @@
 Align word and IPA to pairs of character and phoneme
 """
 from src.config import VOWELS, CONSONANTS, NONE, STRESS
+from src.preprocess import normalize
 
 def align_word(word: str, ipa: str) -> list[tuple[str, str]]:
+    word = normalize(word)
+    
     pairs = []
     
     word_index = 0
